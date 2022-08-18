190e3f4d9e7dfbfba1600531fcd8f08a1d5b6112
CodeAndrogynous <88546992+CodeAndrogynous@users.noreply.github.com>
2022-08-19 00:26:20 +0800

    Update db-connections.js

--- db-connections.js
+++ db-connections.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'database-2.ccbqtvifzmrg.us-west-2.rds.amazonaws.com',
     port: '3306',
-    user: 'root',
-    password: 'cdev',
+    user: 'admin',
+    password: 'tG059014',
     database: 'restaurant_review'
 });
 
